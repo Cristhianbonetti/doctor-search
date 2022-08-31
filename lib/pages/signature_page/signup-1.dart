af451e3f3e61649f0e7bc6997ce3da646bd1ecb2
Cristiano Bonetti <cristhianbonetti@gmail.com>
2022-08-30 21:34:18 -0300

    Ajusting TextFormField

--- lib/pages/signature_page/signup-1.dart
+++ lib/pages/signature_page/signup-1.dart
@@ -5,7 +5,9 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Signup extends StatelessWidget {
-  const Signup({Key? key}) : super(key: key);
+  Signup({Key? key}) : super(key: key);
+  var emailController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -41,134 +43,161 @@ class Signup extends StatelessWidget {
             const SizedBox(
               height: 4,
             ),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'joão da silva',
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: kSecomdaryColor,
-                ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Data de Nascimento',
-                style: TextStyle(
-                    fontFamily: 'Arial', fontSize: 16, color: kSecomdaryColor),
-              ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            const TextField(
-              keyboardType: TextInputType.datetime,
-              decoration: InputDecoration(
-                hintText: 'DD/MM/YYYY',
-                prefixIcon: Icon(
-                  Icons.calendar_month,
-                  color: kSecomdaryColor,
-                ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Gênero',
-                style: TextStyle(
-                    fontFamily: 'Arial', fontSize: 16, color: kSecomdaryColor),
-              ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Selecionar',
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: kSecomdaryColor,
-                ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'E-mail',
-                style: TextStyle(
-                    fontFamily: 'Arial', fontSize: 16, color: kSecomdaryColor),
-              ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            const TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: 'name@exemplo.com.br',
-                prefixIcon: Icon(
-                  Icons.email,
-                  color: kSecomdaryColor,
-                ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Telêfone',
-                style: TextStyle(
-                    fontFamily: 'Arial', fontSize: 16, color: kSecomdaryColor),
-              ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            const TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                hintText: '(31) 99999-9999',
-                prefixIcon: Icon(
-                  Icons.phone,
-                  color: kSecomdaryColor,
-                ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/login-page');
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: kSecomdaryColor,
-                  elevation: 10,
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.fromLTRB(142, 8, 142, 8),
-                  child: Text(
-                    'Continuar',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+            Container(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'joão da silva',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: kSecomdaryColor,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Data de Nascimento',
+                        style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 16,
+                            color: kSecomdaryColor),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.datetime,
+                      decoration: const InputDecoration(
+                        hintText: 'DD/MM/YYYY',
+                        prefixIcon: Icon(
+                          Icons.calendar_month,
+                          color: kSecomdaryColor,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Gênero',
+                        style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 16,
+                            color: kSecomdaryColor),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Selecionar',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: kSecomdaryColor,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'E-mail',
+                        style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 16,
+                            color: kSecomdaryColor),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value == null || !value.contains('@')) {
+                          return 'Email Invalido';
+                        }
+                        return null;
+                      },
+                      // autovalidateMode: AutovalidateMode.always,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: 'name@exemplo.com.br',
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: kSecomdaryColor,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Telêfone',
+                        style: TextStyle(
+                            fontFamily: 'Arial',
+                            fontSize: 16,
+                            color: kSecomdaryColor),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                        hintText: '(31) 99999-9999',
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: kSecomdaryColor,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(24),
+                      child: TextButton(
+                        onPressed: () {
+                          if (formKey.currentState?.validate() ?? false) {
+                            Navigator.of(context).pushNamed('/login-page');
+                          }
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: kSecomdaryColor,
+                          elevation: 10,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.fromLTRB(142, 8, 142, 8),
+                          child: Text(
+                            'Continuar',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
